43d31d39ffbb18114584b75f5ccaaa2194c6959f
cbspace <24204459+cbspace@users.noreply.github.com>
2025-01-26 13:58:51 +1000

    Create Datasets

--- src/baseline/data.py
+++ src/baseline/data.py
@@ -1,7 +1,13 @@
 from datasets import load_dataset
+import pprint
 
 # HuggingFace smoltalk dataset (~4GB)
-dataset = load_dataset('HuggingFaceTB/smoltalk', 'all', split='train')
+train_dataset = load_dataset('HuggingFaceTB/smoltalk', 'all', split='train[:70%]', num_proc=9)
+validation_dataset = load_dataset('HuggingFaceTB/smoltalk', 'all', split='train[70%:90%]', num_proc=9)
+test_dataset = load_dataset('HuggingFaceTB/smoltalk', 'all', split='train[-10%:]', num_proc=9)
 
-# Wikipedia dataset (~72GB)
+# Wikipedia dataset (~72GB) [Future Use]
 #dataset = load_dataset('wikimedia/wikipedia', '20231101.en')
+
+# View a sample of the dataset
+pprint.pp(train_dataset[100])
